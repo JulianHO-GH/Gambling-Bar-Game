1a35107cb0dc6e730a4b26ac06553edcd6f48ac0
JulianUFPS <juliancitohernandez2006@gmail.com>
2025-05-29 12:56:45 -0500

    tryfix4

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ admin.initializeApp()
 
 exports.tryLevel = functions.https.onCall(async (data, context) => {
   const maxLevel = 8
-  const currentLevel = Number(data.currentLevel) || 0
+  let currentLevel = Number(data.currentLevel) || 0
 
   console.log('--- tryLevel v20250528 ---') // Identificador único
   console.log(`Input: currentLevel=${currentLevel}, type=${typeof currentLevel}`)
@@ -28,6 +28,7 @@ exports.tryLevel = functions.https.onCall(async (data, context) => {
   console.log(`Evaluando: success=${success}, currentLevel=${currentLevel}, maxLevel=${maxLevel}`)
   if (success && currentLevel < maxLevel) {
     newLevel = currentLevel + 1
+    currentLevel = newLevel
     status = `Avanzaste al nivel ${newLevel}`
     console.log(`Éxito: newLevel=${newLevel}, status=${status}`)
   } else if (success && currentLevel === maxLevel) {
